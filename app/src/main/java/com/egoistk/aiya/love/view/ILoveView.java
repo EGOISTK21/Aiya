518c6f2ff4557eba0a5df7473bd11fa0e3dfe91f
EGOISTK <chinazhjjq@gmail.com>
2017-03-27 23:52:02 +0800

    删除原来的com包

--- app/src/main/java/com/egoistk/aiya/love/view/ILoveView.java
+++
@@ -1,9 +0,0 @@
-package com.egoistk.aiya.love.view;
-
-/**
- * Created by EGOISTK on 2017/3/17.
- */
-
-public interface ILoveView {
-    void onChangeSwitchsStatus(boolean isChecked, int index);
-}
