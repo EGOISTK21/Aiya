30da038872fd3cf102485625c39584c370c795b8
EGOISTK21 <egoistk21@gmail.com>
2017-04-13 19:39:39 +0800

    删除一些历史遗留activity

---
+++ app/src/main/java/com/aiyaschool/aiya/activity/SignUpActivity.java
@@ -0,0 +1,25 @@
+package com.aiyaschool.aiya.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aiyaschool.aiya.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initListener() {
+
+    }
+}
