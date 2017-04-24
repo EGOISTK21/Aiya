02fe3c550284c796a0bf2788ba48346fa6d76e23
wewarrios <1214597188@QQ.com>
2017-04-24 23:39:14 +0800

    城市选择

--- app/src/main/java/com/aiyaschool/aiya/me/bean/RegionModel.java
+++ app/src/main/java/com/aiyaschool/aiya/me/bean/RegionModel.java
@@ -6,6 +6,32 @@ package com.aiyaschool.aiya.me.bean;
  */
 
 public class RegionModel {
+
     private Long id;
     private Long pid;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/aiyaschool/aiya/me/db/RegionDao.java
+++ app/src/main/java/com/aiyaschool/aiya/me/db/RegionDao.java
@@ -3,6 +3,10 @@ package com.aiyaschool.aiya.me.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.aiyaschool.aiya.me.bean.RegionModel;
+
+import java.util.List;
+
 /**
  * Created by wewarriors on 2017/4/24.
  */
@@ -15,5 +19,13 @@ public class RegionDao {
     public RegionDao(Context context){
         this.mContext = context;
         this.db = RegionDbHelper.getInstance(context).getReadableDatabase();
+    }
+
+    /**
+     * 加载省份
+     * @return
+     */
+    private List<RegionModel> loadProvinceList(){
+        return null;
     }
 }
